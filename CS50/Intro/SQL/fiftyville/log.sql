5c494a4ebe5c340201f88bc1be84e4107ffb158e
Durgesh <durgeshbabu.girajala@gmail.com>
2022-06-12 00:21:38 +0530

    Sun 12 Jun 2022 00:21:38 IST
    Former-commit-id: 1160f397c808e9cef9c9693fe8d52ecd332c6232
    Former-commit-id: bc48012781e69d93e9de759e886d434a428f7252

--- CS50/Intro/SQL/fiftyville/log.sql
+++ CS50/Intro/SQL/fiftyville/log.sql
@@ -3,3 +3,5 @@
 SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2020 AND street = "Chamberlin Street";
 
 SELECT * FROM interviews WHERE year = 2020 AND day = 28 AND month = 7 AND transcript LIKE "%courthouse%";
+
+SELECT * FROM courthouse_security_logs WHERE year = 2020 AND month = 7 AND day = 28 AND hour = 10 AND (minute >= 15 AND minute <= 25);
