6f4df443f79984d97ad33e6a539da337fdfa0ef7
Durgesh <durgeshbabu.girajala@gmail.com>
2022-06-11 23:37:33 +0530

    Sat 11 Jun 2022 23:37:33 IST
    Former-commit-id: 8a2510625257852bbd638c47b315528d5d16c701
    Former-commit-id: 1046e8afb2422a2676c0d0ced3af469edc6263a3

---
+++ CS50/Intro/SQL/movies/13.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT(name) FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE name != "Kevin Bacon" AND title IN (
+    SELECT title FROM people
+    JOIN stars ON people.id = stars.person_id
+    JOIN movies ON stars.movie_id = movies.id
+    WHERE name = "Kevin Bacon" AND birth = 1958
+);
