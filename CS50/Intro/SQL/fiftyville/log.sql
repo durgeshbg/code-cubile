891404666cb82d3d6da54ca0710145bec3a9d947
Durgesh <durgeshbabu.girajala@gmail.com>
2022-06-12 12:15:58 +0530

    Sun 12 Jun 2022 12:15:58 IST
    Former-commit-id: c6950679c04c2a7484aa621af0112aafe90b240d
    Former-commit-id: 35888e145f3cae97b498b538e0242c454a060400

--- CS50/Intro/SQL/fiftyville/log.sql
+++ CS50/Intro/SQL/fiftyville/log.sql
@@ -1,5 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Crime Reports
 SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2020 AND street = "Chamberlin Street";
 
 SELECT * FROM interviews WHERE year = 2020 AND day = 28 AND month = 7 AND transcript LIKE "%courthouse%";
