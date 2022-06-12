0cc5e5b9e97afa60e6d7629cff970d2b07df300b
Durgesh <durgeshbabu.girajala@gmail.com>
2022-06-12 12:38:32 +0530

    Sun 12 Jun 2022 12:38:32 IST
    Former-commit-id: 2b1f252173432a1283da344992bc6cf797a8cd8b
    Former-commit-id: dc453d83881a05fc9b9aca362ab2daa453b5cd05

--- CS50/Intro/SQL/fiftyville/log.sql
+++ CS50/Intro/SQL/fiftyville/log.sql
@@ -39,3 +39,36 @@ SELECT * FROM bank_accounts WHERE account_number IN (
     atm_location = "Fifer Street" AND 
     transaction_type = "withdraw"
 );
+
+-- Name from the people who match the data below
+SELECT name FROM people WHERE 
+
+-- People who spoke on the day and time of the theft
+phone_number IN (
+    SELECT caller FROM phone_calls WHERE year = 2020 AND day = 28 AND month = 7 AND duration <= 60
+) AND
+
+-- People who traveled in the quickest flight on the next of the theft
+passport_number IN (
+    SELECT passport_number FROM passengers WHERE flight_id = (
+    SELECT id FROM flights WHERE year = 2020 AND day = 29 AND month = 7 AND origin_airport_id = (
+        SELECT id FROM airports WHERE city="Fiftyville"
+        ) 
+    ORDER BY hour LIMIT 1
+    )
+) AND
+
+-- License plate of the cars that left the court house after the ten minutes of the theft 
+license_plate IN (
+    SELECT license_plate FROM courthouse_security_logs WHERE year = 2020 AND month = 7 AND day = 28 AND hour = 10 AND (minute >= 15 AND minute <= 25)
+) AND
+
+-- ID of the person who performed transactions on the day of the theft
+id IN (
+    SELECT person_id FROM bank_accounts WHERE account_number IN (
+        SELECT account_number FROM atm_transactions WHERE year = 2020 AND 
+        day = 28 AND month = 7 AND 
+        atm_location = "Fifer Street" AND 
+        transaction_type = "withdraw"
+    )
+);
