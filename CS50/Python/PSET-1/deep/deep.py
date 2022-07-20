f95adef80cbe8e5d9d20d2dda01f06f38057f798
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-07-20 18:09:44 +0530

    Wed 20 Jul 2022 18:09:44 IST
    Former-commit-id: 24fbe35d150dc2d3522644b42f04289107b6e49d
    Former-commit-id: fbe17dfd3e833f042b4a49b045c127682759624f

---
+++ CS50/Python/PSET-1/deep/deep.py
@@ -0,0 +1,5 @@
+ask = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+if ask == '42' or ask.lower() == 'forty two' or ask.lower() == 'forty-two' :
+    print("Yes")
+else:
+    print("No")
