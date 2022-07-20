cf672a43c25c085c5e91679e8f26ecc04015e826
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-07-20 19:35:59 +0530

    Wed 20 Jul 2022 19:35:59 IST
    Former-commit-id: 228bbdf535f704294a71f2d571b6c7ae6d8adb8b
    Former-commit-id: dddf4f20d4e6473e1a2653f5d6657168503c443a

---
+++ CS50/Python/PSET-1/extensions/extensions.py
@@ -0,0 +1,20 @@
+file_name = input("File name: ").split(".")
+extension = file_name[-1]
+
+match extension:
+    case 'gif':
+        print('image/gif')
+    case 'jpg':
+        print('image/jpeg')
+    case 'jpeg':
+        print('image/jpeg')
+    case 'png':
+        print('image/png')
+    case 'pdf':
+        print('application/pdf')
+    case 'txt':
+        print('text/plain')
+    case 'zip':
+        print('application/zip')
+    case _:
+        print("application/octet-stream")
