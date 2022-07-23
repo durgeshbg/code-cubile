1515e039803bb8d5696f45c37c2f54dbc2adc4f6
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-07-24 00:02:55 +0530

    Sun 24 Jul 2022 00:02:55 IST
    Former-commit-id: 0a91296254c631562cadc4b66175d11f6c0e0cd3
    Former-commit-id: d9700050c32be4ab156e12ac47910eab51e4e3eb

---
+++ CS50/Python/PSET-4/figlet/figlet.py
@@ -0,0 +1,24 @@
+import sys
+from pyfiglet import Figlet
+import random
+
+figlet = Figlet()
+fonts = figlet.getFonts()
+
+if len(sys.argv) == 3:
+    if sys.argv[1] == '-f' or sys.argv[1] == '--font':
+        f = sys.argv[2]
+        if f not in fonts:
+            sys.exit('Invalid usage')
+        figlet.setFont(font=f)
+    else:
+        sys.exit('Invalid usage')
+elif len(sys.argv) == 0:
+    f = random.choice(fonts)
+    figlet.setFont(font=f)
+else:
+    sys.exit('Invalid usage')
+
+s = input('Input: ')
+print('Output: ')
+print(figlet.renderText(s))
