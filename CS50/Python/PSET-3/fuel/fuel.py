fd0b3e703ad9f2a1d3439a0ac9c31e05f728a732
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-07-23 21:55:50 +0530

    Sat 23 Jul 2022 21:55:50 IST
    Former-commit-id: bd2bda9e13eb0d0eb8f6df7f0a65729863925f1e
    Former-commit-id: 8691ece978c7b7ccc6d68f85ba2931f72209c4ed

---
+++ CS50/Python/PSET-3/fuel/fuel.py
@@ -0,0 +1,20 @@
+while True:
+    try:
+        fraction = input('Fraction: ').split('/')
+        numerator = int(fraction[0])
+        denominator = int(fraction[1])
+        
+        if numerator > denominator:
+            continue
+        
+        percentage = round((numerator/denominator) * 100)
+        if percentage <= 1:
+            print('E')
+        elif percentage >= 99:
+            print('F')
+        else:
+            print(f'{percentage}%')
+        break
+    
+    except (ValueError,ZeroDivisionError):
+        continue
