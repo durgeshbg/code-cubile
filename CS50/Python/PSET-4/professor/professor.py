3a297f44f5f956ace89fbc0300974b3749a994b6
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-07-27 23:27:08 +0530

    Wed 27 Jul 2022 23:27:08 IST
    Former-commit-id: dc4eb75b5dd4417987f9184d31841a9bb0bee219
    Former-commit-id: d6de955fc2d4767487a1c77aeae488e6e40dd31f

--- CS50/Python/PSET-4/professor/professor.py
+++ CS50/Python/PSET-4/professor/professor.py
@@ -1,22 +1,51 @@
 import random
 
-
 def main():
     level = get_level('Level: ')
+    score = 0
+    for i in range(10):
+        trail = 0
+        x, y, result = generate_integer(level)
 
+        while True:
+            try:
+                print(f'{x} + {y} = ',end='')
+                res = int(input())
+            except ValueError:
+                print('EEE')
+                trail += 1
+                if trail == 3:
+                    print(f'{x} + {y} = {result}')
+                    break
+                continue
+            if result == res:
+                score +=1
+                break
+            else:
+                print('EEE')
+                trail += 1
+                if trail == 3:
+                    print(f'{x} + {y} = {result}')
+                    break
+
+    print(f'Score: {score}')
 
 def get_level(s):
     while True:
         try:
-            l = int(input(s))
+            level = int(input(s))
         except ValueError:
             continue
-        if l in [1,2,3]:
-            return l
+        if level in [1,2,3]:
+            return level
 
 
 def generate_integer(level):
-    ...
+    a = pow(10, level - 1)
+    b = pow(10, level) - 1
+    x = random.randint(a, b)
+    y = random.randint(a, b)
+    return x, y, x + y
 
 
 if __name__ == "__main__":
