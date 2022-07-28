96c560e2da3ce469a4eeb58374e38098239f89f8
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-07-28 23:45:08 +0530

    Thu 28 Jul 2022 23:45:08 IST
    Former-commit-id: 2ba9f94d9dbac18c3b0c99ee90d059312db0b3a8
    Former-commit-id: 18e0a32c15683dd6b36b148f211acfc98655e73c

--- CS50/Python/PSET-5/test_twttr/twttr.py
+++ CS50/Python/PSET-5/test_twttr/twttr.py
@@ -1,10 +1,19 @@
-twttr = input('Input: ')
-output = ''
+def main():
+    twttr = input('Input: ')
+    output = shorten(twttr)
+    print(f'Output: {output}')
 
-for c in twttr:
-    if c.lower() in ['a','e','i','o','u']:
-        continue
-    else:
-        output += c
 
-print(f'Output: {output}')
+
+def shorten(word):
+    twt_str = ''
+    for c in word:
+        if c.lower() in ['a','e','i','o','u']:
+            continue
+        else:
+            twt_str += c
+    return twt_str
+
+
+if __name__ == "__main__":
+    main()
