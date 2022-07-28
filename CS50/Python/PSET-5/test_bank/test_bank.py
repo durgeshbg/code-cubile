bb9f3375a4c93533428348dc8a124b6aaffc5466
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-07-29 00:17:17 +0530

    Fri 29 Jul 2022 00:17:17 IST
    Former-commit-id: dde2cdf5217213a0f1a24d6c4230dca170b99f9c
    Former-commit-id: 7412ce4aa8b00a92e7c2bc5b1c3950d449b7f67c

---
+++ CS50/Python/PSET-5/test_bank/test_bank.py
@@ -0,0 +1,12 @@
+from bank import value
+
+def test_0():
+    assert value('Hello Buddy') == 0
+
+
+def test_20():
+    assert value('Hi Lisa') == 20
+
+
+def test_100():
+    assert value('What\'s up!') == 100
