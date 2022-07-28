e976b4d56329092294d1a499e44df02fc466c6da
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-07-29 00:07:40 +0530

    Fri 29 Jul 2022 00:07:40 IST
    Former-commit-id: 94064c02e34820077c056a00ec6bab0633eea9a4
    Former-commit-id: d14be44190d18ec3469963aa51d02a45cc0fb2c9

--- CS50/Python/PSET-5/test_bank/bank.py
+++ CS50/Python/PSET-5/test_bank/bank.py
@@ -1,8 +1,18 @@
-greet = input("Greeting: ").strip().lower().split(" ")
-first_word = greet[0]
-if first_word == 'hello' or first_word == 'hello,':
-    print("$0")
-elif 'h' == first_word[0] :
-    print("$20")
-else:
-    print("$100")
+def main():
+    greet = input("Greeting: ").strip().lower().split(" ")
+    money = value(greet)
+    print(f'${money}')
+
+
+def value(greeting):
+    first_word = greeting[0]
+    if first_word == 'hello' or first_word == 'hello,':
+        return 0
+    elif 'h' == first_word[0] :
+        return 20
+    else:
+        return 100
+
+
+if __name__ == "__main__":
+    main()
