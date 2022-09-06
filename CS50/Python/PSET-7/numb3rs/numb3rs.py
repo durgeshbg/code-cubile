71ec35f6c6c7137ab5053e06456c38d0ec5c4114
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-09-06 23:25:36 +0530

    Tue  6 Sep 2022 23:25:36 IST
    Former-commit-id: 0bd91c65b941d3579a49246f807c1b41afd96490
    Former-commit-id: f919f2fb7d40790b563365babb68c4d4e900b788
    Former-commit-id: 98d3a0b88e803ca3401fb37b0233590dcb3ebd32
    Former-commit-id: ec8bf2d5c508e72df225185a3b1ace687877647d
    Former-commit-id: 0ec350094e06c2b68c3d3dbb08da5e6120b3fdd3

--- CS50/Python/PSET-7/numb3rs/numb3rs.py
+++ CS50/Python/PSET-7/numb3rs/numb3rs.py
@@ -7,11 +7,13 @@ def main():
 
 
 def validate(ip):
-    ...
-
-
-...
-
+    if matches := re.search(r"(\d+)\.(\d+)\.(\d+)\.(\d+)", ip):
+        for i in range(4):
+            if int(matches.group(i+1)) > 255:
+                return False
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
