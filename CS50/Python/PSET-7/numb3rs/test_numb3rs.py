18f327633b66d3be8c7298dc1ce87068380003a3
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-09-07 10:50:37 +0530

    Wed Sep  7 10:50:37 IST 2022
    Former-commit-id: e6c776a9d837ed13d6fd4b0c526136241154d73b
    Former-commit-id: 28ae62b359d6dab44fa31cf476f197c7da0ea624
    Former-commit-id: f9d75a2693d5cc9a1f54f4160ef43d7b7c294857
    Former-commit-id: 661358e7c38e0be581a1d12110f9947959ea06ee
    Former-commit-id: 7a5ab7439a279af8596e7a7aa58e43a921b498fe

--- CS50/Python/PSET-7/numb3rs/test_numb3rs.py
+++ CS50/Python/PSET-7/numb3rs/test_numb3rs.py
@@ -5,3 +5,19 @@ def test_non_numbers():
 
 def test_number():
     assert validate("122.13.14.36") == True
+
+def test_byte1():
+    assert byte1("234") == True
+    assert byte1("278") == False
+
+def test_byte2():
+    assert byte2("234") == True
+    assert byte2("278") == False
+
+def test_byte3():
+    assert byte3("234") == True
+    assert byte3("278") == False
+
+def test_byte4():
+    assert byte4("234") == True
+    assert byte4("278") == False
