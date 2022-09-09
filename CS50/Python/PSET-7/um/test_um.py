dc206661a4d5a1631e09356c4443a31a7bd8d206
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-09-09 13:03:56 +0530

    Fri  9 Sep 2022 13:03:56 IST
    Former-commit-id: f24a622ac753e526ce158c8ae1c8a9af95f9bfd0
    Former-commit-id: 0ddc0438bcd8b311cb46a589569126e13985e49c
    Former-commit-id: 4eeb312e0b70ce077e37fc35c99d761c89ccc039
    Former-commit-id: c95d911f1cd0c99f9c7125b952c0c9be9dfa093c
    Former-commit-id: a60f6f99f8a786d9c7debec9ff0fec748b991f75

--- CS50/Python/PSET-7/um/test_um.py
+++ CS50/Python/PSET-7/um/test_um.py
@@ -2,6 +2,8 @@ from um import count
 
 def test_lowercase():
     assert count("hello, um, world") == 1
+    assert count("um...") == 1
+    assert count("um..um?") == 2
 
 
 if __name__ == "__main__":
