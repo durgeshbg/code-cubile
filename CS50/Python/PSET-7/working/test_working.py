a69d9dfa0f4b637b9a69d5526765722cf8f55e6d
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-09-09 11:42:25 +0530

    Fri  9 Sep 2022 11:42:25 IST
    Former-commit-id: 1ba64e27fc6d6e7687357c5332a168ec3e6bc82a
    Former-commit-id: 84a325c064b4eb27556e06111ebeb1876a4a4060
    Former-commit-id: dc35c42a0de69282f312b0ed80b9f2f12c92ab19
    Former-commit-id: bbeac08af5428fb146b3036526ccad091fc98a16
    Former-commit-id: 1f7aa5125e568bfee4cbd09e84b78617e6891974

--- CS50/Python/PSET-7/working/test_working.py
+++ CS50/Python/PSET-7/working/test_working.py
@@ -1,6 +1,11 @@
 from working import convert
 import pytest
 
-def test_convert():
-    with pytest.raises(ValueError):
-        convert('')
+def test_convert_dayshift():
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("10:30 AM to 4:47 PM") == "10:30 to 16:47"
+    
+def test_convert_nightshift():
+    assert convert("11 PM to 7 AM") == "23:00 to 07:00"
+    assert convert("10:30 PM to 5:47 AM") == "22:30 to 05:47"
+ 
