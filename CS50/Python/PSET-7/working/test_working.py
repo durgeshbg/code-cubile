79104f5958fc3e148047624d3008578e9e123aad
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-09-09 11:43:17 +0530

    Fri  9 Sep 2022 11:43:17 IST
    Former-commit-id: 7ab49ffb5e3bcd1e4423ce995402febdde9f6e86
    Former-commit-id: 0fe95db2ed1766c28659e62e5abad7c55bd8f384
    Former-commit-id: 0c7d1a6acc4b01809e5bf068ad00c1d451435ca6
    Former-commit-id: 6da514e1a5addce65c8cfb02b87c222a41944466
    Former-commit-id: 02e4e2d740eccaa977228603a07dcdc7ac42c2b9

--- CS50/Python/PSET-7/working/test_working.py
+++ CS50/Python/PSET-7/working/test_working.py
@@ -14,4 +14,5 @@ def test_ValueError():
         convert('09:00 AM - 17:00 PM')
     with pytest.raises(ValueError):
         convert('9 AM - 5 PM')
-    
+    with pytest.raises(ValueError):
+        convert('9:60 AM to 5:60 PM')
