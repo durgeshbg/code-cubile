73bd5b2a8e13241c6b8a7ae76c0db91f08f2d7df
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-09-09 13:04:23 +0530

    Fri  9 Sep 2022 13:04:23 IST
    Former-commit-id: 3ca92bdb1b8a8dd05972b1ee133bee8d361bfce4
    Former-commit-id: cb3f90c6ca9d3512339908ee80d541d4820344c8
    Former-commit-id: 7561dad496a17efec64a9670374475d1ab4c7031
    Former-commit-id: 274e8e39fc6a328423c6088f88ab946c96266153
    Former-commit-id: b6ba190dc3fc140026f50f908ad5b0b887596584

--- CS50/Python/PSET-7/um/test_um.py
+++ CS50/Python/PSET-7/um/test_um.py
@@ -5,6 +5,9 @@ def test_lowercase():
     assert count("um...") == 1
     assert count("um..um?") == 2
 
+def test_uppercase():
+    assert count("Um, thanks for the album.") == 1
+    
 
 if __name__ == "__main__":
     main()
