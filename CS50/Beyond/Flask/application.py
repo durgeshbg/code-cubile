e5afb58142f0ca55efafae51c4180c2dbc51cc8d
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-09-22 23:45:46 +0530

    Thu 22 Sep 2022 23:45:45 IST
    Former-commit-id: d074eb164bf7f5d00c1f128740c47280b06036af
    Former-commit-id: 536049618133ddf4bfb776f9045cd58bd24bcf99
    Former-commit-id: 924d768d323d92d50ea48572c4e947dbf705bda7
    Former-commit-id: c1eb90ecd32f0a7d89d16926ccf0669f28de722a
    Former-commit-id: 123522aafe203cf77377f6975f01435252ae8276

--- CS50/Beyond/Flask/application.py
+++ CS50/Beyond/Flask/application.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
@@ -15,4 +16,4 @@ def index():
                             [None, None, None],
                             [None, None, None]]
         session["turn"] = "X"
-    render_template('game.html', board=session[board], turn=session["turn"])
+    return render_template("game.html", board=session["board"], turn=session["turn"])
