52cff59e51c7f19ad68e34a6eccb2ad533dca010
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-09-25 23:42:37 +0530

    Sun 25 Sep 2022 23:42:37 IST
    Former-commit-id: 591d668af4b46455422a371bb1d5b82388aa2514
    Former-commit-id: 51f477e7e1daa964c5bf28260d92052b3c4004a5
    Former-commit-id: ce9492b43efb544720c503dd75f6709dc23f25d7
    Former-commit-id: 26744121ea7043d9b2442ba06cb019fc1aa9fb6f
    Former-commit-id: 02071a3b6113fb1d1b72a57555d63db36715c556

--- CS50/Python/PSET-8/seasons/seasons.py
+++ CS50/Python/PSET-8/seasons/seasons.py
@@ -5,8 +5,8 @@ def main():
     ...
 
 
-...
-
+def leapyear(y):
+    return ((True if (y % 400 == 0) else False) if (y % 100 == 0) else True) if (y % 4 == 0) else False
 
 if __name__ == "__main__":
     main()
