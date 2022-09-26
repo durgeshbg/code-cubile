dbec6b1481c87306cc033e968186438be2bd9846
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-09-26 23:20:05 +0530

    Mon 26 Sep 2022 23:20:05 IST
    Former-commit-id: 59a9a29d05e99384bdcdf05001669982caa5335d
    Former-commit-id: be69f13fb965e39bcf60394f3cc311a83978b456
    Former-commit-id: ae8646349c122b6b53d27b83a39c495bc46e274f
    Former-commit-id: 3f0ba9ce27d82968ed02a1bd89e5b2d2ec22a3df
    Former-commit-id: 2ac7ed7d1647a25a4f4ae0fdb91c65dd008930bd

--- CS50/Python/PSET-8/jar/jar.py
+++ CS50/Python/PSET-8/jar/jar.py
@@ -1,20 +1,35 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        self.capacity = capacity
+        self._size = 0
 
     def __str__(self):
-        ...
+        return "🍪" * self.size
 
     def deposit(self, n):
-        ...
+        self.size += n
+        if self.size > self.capacity:
+            raise ValueError()
 
     def withdraw(self, n):
-        ...
+        self.size -= n
+        if self.size < 0:
+            raise ValueError()
 
     @property
     def capacity(self):
-        ...
+        return self._capacity
+
+    @capacity.setter
+    def capacity(self, capacity):
+        if capacity < 0:
+            raise ValueError()
+        self._capacity = capacity
 
     @property
     def size(self):
-        ...
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        self._size = size
