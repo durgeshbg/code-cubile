968c22cb4257fff46a19000f7fde1e77b812dc63
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-09-26 21:01:39 +0530

    Mon 26 Sep 2022 21:01:39 IST
    Former-commit-id: e5c66e543762b7d4412900f23413704dd4d203cb
    Former-commit-id: c4e0050b9b6211f452fce0da5eb6416f9a352da2
    Former-commit-id: 9b9bf3db0184f745b03268a04c72b5f418401e1e
    Former-commit-id: 87edbe2c11fbcc5ce3e238c71035945be50ab7b7
    Former-commit-id: 77232b8422517237e12bde709c34cf8a22cc9727

--- CS50/Python/PSET-8/seasons/test_seasons.py
+++ CS50/Python/PSET-8/seasons/test_seasons.py
@@ -1,5 +1,14 @@
 from seasons import convert, inputdate
+import pytest
 
 def test_convert():
     assert convert(2020, 9, 23) == 1055520
     assert convert(2001, 10, 20) == 11010240
+
+def test_inputdate():
+    with pytest.raises(SystemExit):
+        inputdate("23-Jan-2000")
+    with pytest.raises(SystemExit):
+        inputdate("22 Feb 2000")
+    with pytest.raises(SystemExit):
+        inputdate("")
