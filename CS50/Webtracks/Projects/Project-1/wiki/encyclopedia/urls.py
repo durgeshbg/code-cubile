b19cef646b320365b5fd543ce8b72655d321bd53
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-02-14 17:11:35 +0530

    complete project-1 in CS50/WebTracks
    Former-commit-id: 235bba8e1f8993881dada3a744099927bf7282ef
    Former-commit-id: 8b6080f20966d564fb48592160e957d3a158b67d
    Former-commit-id: 31cfbff4a66952d706f200d544ab8f70220eb3cc
    Former-commit-id: 6098a17988e2add3f07ba7e74dc05d3128aad5e0
    Former-commit-id: 2865e757aeb9ca707dbe9112fb327276dbdebedb

--- CS50/Webtracks/Projects/Project-1/wiki/encyclopedia/urls.py
+++ CS50/Webtracks/Projects/Project-1/wiki/encyclopedia/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("add", views.add, name="add"),
+    path("random_entry",views.random_entry, name="random_entry"),
+    path("edit/<str:title>",views.edit, name="edit")
 ]
---
+++ CS50/Webtracks/Projects/Project-1/wiki/entries/JAVA.md
@@ -0,0 +1,3 @@
+# JAVA
+
+Javass is a general purpose programming language. It is similar to the [Python](/wiki/Python) language.
