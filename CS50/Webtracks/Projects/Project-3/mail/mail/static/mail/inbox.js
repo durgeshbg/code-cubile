6423e94c71d75949f005534e9dc162ddcd6a4985
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-04-24 21:23:01 +0530

    add send mail function in mail app

--- CS50/Webtracks/Projects/Project-3/mail/mail/static/mail/inbox.js
+++ CS50/Webtracks/Projects/Project-3/mail/mail/static/mail/inbox.js
@@ -6,20 +6,25 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // Post mail
+  document.querySelector("form").addEventListener('submit', send_mail);
+
   // By default, load the inbox
   load_mailbox('inbox');
+
 });
 
 function compose_email() {
-
+  
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-
+  
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +35,27 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_mail(event) {
+  const cmpRecipients = document.querySelector('#compose-recipients').value;
+  const cmpSubject = document.querySelector('#compose-subject').value;
+  const cmpBody = document.querySelector('#compose-body').value;
+  event.preventDefault();
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: cmpRecipients,
+        subject: cmpSubject,
+        body: cmpBody
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      if (result) {
+        document.querySelector('#emails-view').innerHTML = `<h3>${result.error}</h3>`;
+      } else {
+        load_mailbox("sent")
+      }
+  });
 }
