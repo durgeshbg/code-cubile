c52c338cebd774bb295ae4a5d42df000f8f8a747
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-05 11:20:52 +0530

    Tue Dec  5 11:20:52 IST 2023

--- TheOdinProject/js-modules/webpack.config.js
+++ TheOdinProject/js-modules/webpack.config.js
@@ -7,9 +7,4 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist'),
-        }
-    },
 };
