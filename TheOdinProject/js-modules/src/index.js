dfbca4ef3880bbea1b8619a3b5834eaad8b01ee1
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-05 19:46:56 +0530

    Tue Dec  5 19:46:56 IST 2023

--- TheOdinProject/js-modules/src/index.js
+++ TheOdinProject/js-modules/src/index.js
@@ -2,10 +2,6 @@ import myName from './myName';
 import './style.css';
 import Icon from './icon.png';
 import Data from './data.csv';
-import Notes from './data.xml';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json5 from './data.json5';
 
 function component() {
     const element = document.createElement('div');
@@ -16,10 +12,6 @@ function component() {
     element.appendChild(myImage);
 
     console.log(Data);
-    console.log(Notes);
-    console.log(toml);
-    console.log(yaml);
-    console.log(json5);
     return element;
 }
 
