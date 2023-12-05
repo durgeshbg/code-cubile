9b2be76975924ab6459088618e21bf2bd1a92a82
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-05 18:22:41 +0530

    Tue Dec  5 18:22:41 IST 2023

--- TheOdinProject/js-modules/src/index.js
+++ TheOdinProject/js-modules/src/index.js
@@ -1,10 +1,11 @@
-import myName from "./myName";
+import myName from './myName';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
-    element.innerHTML = myName('John')
+    element.innerHTML = myName('John');
+    element.classList.add('hello');
     return element;
-    
 }
 
 document.body.appendChild(component());
